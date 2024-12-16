94ce095789b7c9b3eb1db64eab8dce7ba7837b26
marialtandzhiyan3 <qy310492@gmail.com>
2024-12-16 21:59:58 +0200

    Add project files.

---
+++ UPpr_24-25_fn0MI0600486_KP06/UPpr_24-25_fn0MI0600486_KP06.cpp
@@ -0,0 +1,22 @@
+/** 
+* 
+* Solution to course project # 6
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2024/2025
+* 
+* @author Mari Altandzhiyan
+* @idnumber 0MI0600486 -  GCC или VC
+* 
+* <предназначение на кода>
+* 
+**/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+}
+
